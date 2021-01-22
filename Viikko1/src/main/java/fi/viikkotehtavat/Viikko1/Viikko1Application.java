224fbb46d06ad5753e4624716348594634fe049c
minnaok <minnoksa@gmail.com>
2021-01-22 18:57:21 +0200

    first commit

---
+++ Viikko1/src/main/java/fi/viikkotehtavat/Viikko1/Viikko1Application.java
@@ -0,0 +1,13 @@
+package fi.viikkotehtavat.Viikko1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Viikko1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Viikko1Application.class, args);
+	}
+
+}
---
+++ Viikko1/src/test/java/fi/viikkotehtavat/Viikko1/Viikko1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.viikkotehtavat.Viikko1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viikko1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
